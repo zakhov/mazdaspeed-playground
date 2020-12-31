5e31585789c5b469373caa5b8d26fb6509f05a39
zakhov <zakhov.vladimir@gmail.com>
2020-12-31 20:21:30 +0800

    refactor: synced project structure with create-pandajs-game repo

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp')
 var htmlreplace = require('gulp-html-replace')
 
 gulp.task('buildIndex', function (done) {
-    gulp.src('index.html')
+    gulp.src('game/index.html')
         .pipe(
             htmlreplace({
                 js: 'game.min.js',
@@ -13,7 +13,11 @@ gulp.task('buildIndex', function (done) {
 })
 
 gulp.task('copyAssets', function (done) {
-    gulp.src('./media/**/*.{png,json,atlas,ogg,mp4,jpg,manifest}').pipe(gulp.dest('public/media'))
+    gulp.src('game/media/**/*.{png,json,atlas,ogg,mp4,jpg,manifest}').pipe(gulp.dest('public/media'))
+    done()
+})
 
+gulp.task('initFavicons', function (done) {
+    gulp.src('scripts/favicons/*.{png,manifest}').pipe(gulp.dest('game/media/favicons'))
     done()
 })
